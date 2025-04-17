532384c1de0f81152bb3b98b30c7cda2de19e959
Samitha Perera <73548027+SamithaSandeepa@users.noreply.github.com>
2025-04-17 23:32:42 +0530

    Update app.js

--- backend/src/app.js
+++ backend/src/app.js
@@ -6,27 +6,19 @@ import matchRoutes from "./routes/matchRoutes.js";
 
 const app = express();
 
-const allowed = [
+const allowedOrigins = [
   "http://localhost:3000",
-  "https://sports-streaming-platform-prototype.vercel.app/",
   "https://sports-streaming-platform-prototype.vercel.app",
-  
 ];
 
 app.use(
   cors({
-    origin: (origin, cb) => {
-      // allow REST clients like Postman with no origin:
-      if (!origin) return cb(null, true);
-      if (allowed.includes(origin)) return cb(null, true);
-      cb(new Error("Not allowed by CORS"));
-    },
+    origin: allowedOrigins,
     credentials: true,
   })
 );
 
 app.use(express.json());
-
 app.use("/api/auth", authRoutes);
 app.use("/api/matches", matchRoutes);
 
