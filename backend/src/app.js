cdc676f493b568c77ac2f0defc2d5595dfbb46d3
Samitha Perera <73548027+SamithaSandeepa@users.noreply.github.com>
2025-04-17 23:48:42 +0530

    Update app.js

--- backend/src/app.js
+++ backend/src/app.js
@@ -6,24 +6,7 @@ import matchRoutes from "./routes/matchRoutes.js";
 
 const app = express();
 
-const allowedOrigins = [
-  "http://localhost:3000",
-  "https://sports-streaming-platform-prototype.vercel.app",
-];
-app.use(
-  cors({
-    origin: function (origin, callback) {
-      if (!origin || allowedOrigins.includes(origin)) {
-        callback(null, true);
-      } else {
-        callback(new Error("Not allowed by CORS"));
-      }
-    },
-    methods: ["GET", "POST", "PUT", "DELETE"],
-    credentials: true,
-  })
-);
-
+app.use(cors());
 app.use(express.json());
 app.use("/api/auth", authRoutes);
 app.use("/api/matches", matchRoutes);
