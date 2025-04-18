885dfb533303eba0111e1be1f15c9594325b9185
Samitha Perera <73548027+SamithaSandeepa@users.noreply.github.com>
2025-04-18 11:08:18 +0530

    Update app.js

--- backend/src/app.js
+++ backend/src/app.js
@@ -7,8 +7,9 @@ import matchRoutes from "./routes/matchRoutes.js";
 const app = express();
 
 const allowedOrigins = [
-  //   "http://localhost:3000",
+  "http://localhost:3000",
   "https://sports-streaming-platform-prototype.vercel.app",
+  "sports-streaming-platform-protot-git-fd2346-samitha99s-projects.vercel.app",
   "*",
 ];
 app.use(cors({ origin: allowedOrigins }));
